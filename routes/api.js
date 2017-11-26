7504c4529052da52b3c5a89ee7da08f4ec30113d
njerucyrus <njerucyrus5@gmail.com>
2017-11-26 05:06:53 +0300

    added controllers to the routers

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,72 @@
 var express = require('express');
+
+var userController = require('../controllers/userController');
+
+var postController = require('../controllers/postController');
+
+var commentController = require('../controllers/commentController');
+
+var likesController = require('../controllers/likesController');
+
+var commentsCountController = require('../controllers/commentsCountController');
+
 var router = express.Router();
+
+
+router.get('/users', userController.user_list());
+
+router.get('/user/:id', userController.user_detail);
+
+router.post('/user/create', userController.user_create);
+
+router.post('/user/:id/update', userController.user_update);
+
+router.post('/user/:id/delete', userController.user_delete);
+
+
+router.get('/posts', postController.post_list);
+
+router.get('/post/:id', postController.post_detail);
+
+router.post('/post/create', postController.post_create);
+
+router.post('/post/:id/update', postController.post_update);
+router.post('/post/:id/delete', postController.post_delete);
+
+
+router.get('/comments', commentController.comment_list);
+
+router.get('/comment/:id', commentController.comment_detail);
+
+router.post('/comment/create', commentController.comment_create);
+
+router.post('/comment/:id/update', commentController.comment_update);
+
+router.post('/comment/:id/delete', commentController.comment_delete);
+
+
+router.get('/likes', likesController.likes_list);
+
+router.get('/likes/:id',likesController.likes_detail);
+
+router.post('/likes/create', likesController.likes_create);
+
+router.post('/likes/:id/update', likesController.likes_update);
+
+router.post('/likes/:id/delete', likesController.likes_delete);
+
+
+router.get('/comments_count', commentsCountController.comments_count_list);
+
+router.get('/comments_count/:id',commentsCountController.comments_count_detail);
+
+router.post('/comments_count/create', commentsCountController.comments_count_create);
+
+router.post('/comments_count/:id/update', commentsCountController.comments_count_update);
+
+router.post('/comments_count/:id/delete', commentsCountController.comments_count_delete);
+
+
+
+module.exports = router;
+
